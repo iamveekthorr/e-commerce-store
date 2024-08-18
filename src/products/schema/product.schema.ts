ebd22fa4fb08858050f2c041949103a2d36113a0
davy-dray <samdavido722@gmail.com>
2024-08-18 21:04:54 +0100

    feat(entity): set up entitie and relationships

---
+++ src/products/schema/product.schema.ts
@@ -0,0 +1,33 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument, Types } from 'mongoose';
+
+
+export type ProductDocument = HydratedDocument<Product>;
+
+@Schema()
+export class Product {
+    @Prop({ required: true, type: String })
+    prdoductName: string;
+
+    //Refactor to have it's own entity
+    @Prop({ required: true, type: String })
+    category: string;
+
+     /**
+     * The store the product belongs to
+     */
+    @Prop({ required: true, type: Types.ObjectId, ref: 'Store' })
+    store: Types.ObjectId;
+
+    @Prop({ required: true, type: String })
+    description: string;
+
+    @Prop({ required: true, type: Number })
+    price: number;
+
+    @Prop({ required: false, type: Number, default: 0 })
+    stock: number;
+
+}
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
---
+++ src/stores/schema/store.schema.ts
@@ -0,0 +1,23 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument, Types } from 'mongoose';
+
+
+export type StoreDocument = HydratedDocument<Store>;
+
+@Schema()
+export class Store {
+    @Prop({ required: true, type: String })
+    name: string;
+
+    @Prop({ type: String })
+    description?: string;
+
+    /**
+     * Owner of the store
+     */
+    @Prop({ required: true, type: Types.ObjectId, ref: 'User' })
+    store: Types.ObjectId;
+
+}
+
+export const StoreSchema = SchemaFactory.createForClass(Store);
